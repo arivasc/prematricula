c5636adb73a8543c219a03252f076660bdf65f44
Anthony Rivas <arivasc@gmail.com>
2022-08-08 17:02:11 -0500

    settings

--- rematriculas/settings.py
+++ rematriculas/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#7lx!_it+c!0mc74j!@x#)!!n--7m*=dm$-itiufsf1%zyz@tw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
