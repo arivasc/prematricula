eac38b789c5252ba565a37e7ea56ea83671cc055
Anthony Rivas <arivasc@gmail.com>
2022-08-12 19:20:16 -0500

    Usuarios test añadidos

--- Auth/urls.py
+++ Auth/urls.py
@@ -3,9 +3,11 @@ from django.contrib.auth.views import (
     LoginView, LogoutView
 )
 from Auth.views import UserLogin
+from Auth.views import register_from_csv
 from django.urls import path
 
 urlpatterns = [
     path('login/', UserLogin.as_view(), name="login"),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('csv-alumnos/', register_from_csv, name= 'csv-alumnos')
 ]
--- Auth/views.py
+++ Auth/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 
+from django.http import JsonResponse
+from csv import reader
+from Auth.models import User
+from Schools.models import Escuela
+
 from django.contrib.auth.views import LoginView    
 
 class UserLogin(LoginView):
     template_name = 'LoginView_form.html'
+
+def register_from_csv(request):
+    with open('templates/csv/test-ips.csv', encoding="utf8") as csv_file:
+        csvf = reader(csv_file)
+        #_ = next(csvf)
+        for row in csvf:
+            alumno = User()
+            alumno.email = row[7]
+            alumno.cui = row[1]
+            alumno.dni = row[3]
+            alumno.last_name = row[4] + " " + row[5]
+            alumno.name = row[6]
+            escuela = Escuela.objects.get(nombre=row[9])
+            alumno.escuela = escuela
+            alumno.set_password(row[1])
+            alumno.save()
+    
+    return JsonResponse('Creación de alumnos desde csv trabajando...', safe= False)
